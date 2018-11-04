e6692ac14586ff34f03e3f5168e662dd2a76a862
Antoine <zertaps@gmail.com>
2018-11-04 17:54:59 +0100

    Clean project

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource';
+
 Vue.config.productionTip = false
 Vue.use(VueResource);
-//https://docs.coinapi.io/#list-all-assets
 Vue.http.options.root = 'https://api.coinmarketcap.com/v2/';
-//Vue.http.headers.common['X-CoinAPI-Key'] = '67A6161D-F28A-4CAC-944D-8D392FEEC26B';
 Vue.http.headers.common['Accept'] = 'application/json';
 
 Vue.component('app-item', {
